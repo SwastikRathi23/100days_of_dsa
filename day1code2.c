6bd2bf7a78ffe17729f0c72475f7726c254f74f0
SwastikRathi23 <swastik.rathi23@gmail.com>
2026-02-27 09:14:28 +0530

    Add files via upload

---
+++ day1code2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, target;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int nums[n];
+
+    printf("Enter elements: ");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(nums[i] + nums[j] == target) {
+                printf("Output: [%d, %d]\n", i, j);
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
